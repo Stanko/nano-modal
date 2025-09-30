03daae897df30bf23b6305038057786ad6018677
Stanko <stanko.tadic@gmail.com>
2025-09-30 23:47:00 +0200

    Added gc

--- src/demo.ts
+++ src/demo.ts
@@ -64,3 +64,16 @@ document
       }
     });
   });
+
+// Goat counter
+if (import.meta.env.PROD) {
+  const gc = document.createElement("script");
+  gc.setAttribute(
+    "data-goatcounter",
+    "https://muffinman_io.goatcounter.com/count",
+  );
+  gc.setAttribute("async", "");
+  gc.src = "//gc.zgo.at/count.js";
+
+  document.body.appendChild(gc);
+}
